2e2abaaff46df825b7f723dd2de52da1e356e978
Hugo Aragones <aragones.hugo@gmail.com>
2022-05-17 11:50:11 +0200

    update LatestMusic: fetch disabled (malfunction) + added img, subtitle and stream link

--- src/components/LatestMusic.jsx
+++ src/components/LatestMusic.jsx
@@ -4,27 +4,40 @@ import '../styles/Banner.css'
 function LatestMusic() {
 
     /* const [music, setMusic] = useState([])
+    const musicLength = music.data.length
+    const lastIndex = musicLength -1
 
     useEffect(() => {
         fetch('http://localhost:1337/api/musics?populate=cover')
             .then(res => res.json())
             .then(json => setMusic(json))
         }
-    ,[])
+    ,[])  */
+    
 
+
+// TEST API REQUEST EN ASYNC :
+
+/*     const [music, setMusic] = useState([])
     const musicLength = music.data.length
-    const lastIndex = musicLength -1 */
-
-    function handleClick(e) {
-        e.prevenDefault()
-        console.log('clic')
+    const lastIndex = musicLength -1
+    
+    const fetchMusic = async () => {
+        const url = "http://localhost:1337/api/musics?populate=cover"
+        const response = await fetch(url)
+        const json = await response.json()
+        setMusic(json)
     }
+
+    useEffect(() => fetchMusic, []) */
 
     return (
         <div className='LatestMusic'>
-            {/* <img src={`http://localhost:1337${music.data[lastIndex].attributes.cover.data.attributes.formats.small.url}`} alt={`cover-${music.data[lastIndex].attributes.title}`} /> */}
+            {/* <img src={`http://localhost:1337${music.data[lastIndex].attributes.cover.data.attributes.formats.small.url}`} alt={`cover-${music.data[lastIndex].attributes.title}`} />
+            <a href={music.data[lastIndex].attributes.link} >Stream</a> */}
             <img src="http://localhost:1337/uploads/small_miniature_sun_starts_shining_herbe_add77c58d1.jpg" alt="cover-sss-endur" />
-            <button onClick={handleClick} >Stream</button>
+            <h2>Available Now</h2>
+            <a className='btn-test' href="https://open.spotify.com/album/46bZcuev4Gl9xgZGVh2cAu?si=h0m_aIQQRPygUePJBR1h5w&nd=1" >Stream</a>
         </div>
     )
 }
